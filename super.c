261613ab42d0d1b8e203ff488e3671827557cc0c
hzy <h1260071766@126.com>
2022-05-04 01:19:30 +0800

    Day2

--- super.c
+++ super.c
@@ -27,13 +27,114 @@
 
 #include <linux/module.h>
 #include <linux/fs.h>
+#include <linux/pagemap.h>
 #include <linux/version.h>
+#include <linux/nls.h>
+#include <linux/slab.h>
+#include "samplefs.h"
 
 /* helpful if this is different than other fs */
 #define SAMPLEFS_MAGIC     0x73616d70 /* "SAMP" */
 
+static void samplefs_put_super(struct super_block *sb)
+{
+	struct samplefs_sb_info *sfs_sb;
+
+	sfs_sb = SFS_SB(sb);
+	if (sfs_sb == NULL) {
+		/* Empty superblock info passed to unmount */
+		return;
+	}
+
+	unload_nls(sfs_sb->local_nls);
+
+	/* FS-FILLIN your fs specific umount logic here */
+
+	kfree(sfs_sb);
+	return;
+}
+
+
+struct super_operations samplefs_super_ops = {
+	.statfs         = simple_statfs,
+	.drop_inode     = generic_delete_inode, /* Not needed, is the default */
+	.put_super      = samplefs_put_super,
+};
+
+static void samplefs_parse_mount_options(char *options,
+                                         struct samplefs_sb_info *sfs_sb)
+{
+	char *value;
+	char *data;
+	int size;
+
+	if (!options)
+		return;
+
+	while ((data = strsep(&options, ",")) != NULL) {
+		if (!*data)
+			continue;
+		if ((value = strchr(data, '=')) != NULL)
+			*value++ = '\0';
+
+		if (strncasecmp(data, "rsize", 5) == 0) {
+			if (value && *value) {
+				size = simple_strtoul(value, &value, 0);
+				if (size > 0)
+					sfs_sb->rsize = size;
+			}
+		} else if (strncasecmp(data, "wsize", 5) == 0) {
+			if (value && *value) {
+				size = simple_strtoul(value, &value, 0);
+				if (size > 0)
+					sfs_sb->wsize = size;
+			}
+		} /* else unknown mount option */
+	}
+}
+
+
 static int samplefs_fill_super(struct super_block *sb, void *data, int silent)
 {
+	struct inode *inode;
+	struct samplefs_sb_info *sfs_sb;
+
+	sb->s_maxbytes       = MAX_LFS_FILESIZE; /* NB: may be too large for mem */
+	sb->s_blocksize      = PAGE_SIZE;
+	sb->s_blocksize_bits = PAGE_SHIFT;
+	sb->s_magic          = SAMPLEFS_MAGIC;
+	sb->s_op             = &samplefs_super_ops;
+	sb->s_time_gran      = 1; /* 1 nanosecond time granularity */
+
+	/* Eventually replace iget with:
+	inode = samplefs_get_inode(sb, S_IFDIR | 0755, 0); */
+
+	inode = iget_locked(sb, SAMPLEFS_ROOT_I);
+
+	if (!inode)
+		return -ENOMEM;
+
+	sb->s_fs_info = kzalloc(sizeof(struct samplefs_sb_info), GFP_KERNEL);
+	sfs_sb = SFS_SB(sb);
+	if (!sfs_sb) {
+		iput(inode);
+		return -ENOMEM;
+	}
+
+	sb->s_root = d_make_root(inode);
+	if (!sb->s_root) {
+		iput(inode);
+		kfree(sfs_sb);
+		return -ENOMEM;
+	}
+
+	/* below not needed for many fs - but an example of per fs sb data */
+	sfs_sb->local_nls = load_nls_default();
+
+	samplefs_parse_mount_options(data, sfs_sb);
+
+	/* FS-FILLIN your filesystem specific mount logic/checks here */
+
 	return 0;
 }
 
